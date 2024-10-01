27070f24654fdf237b641476656e2e599aab33d8
rodrigocout0 <rodrigospcouto@gmail.com>
2024-09-30 21:54:14 -0300

    resolução da atividade hacker edition jest functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 // Função que soma dois números
-function somar() {}
+function somar(numero1, numero2) {
+  return numero1 + numero2;
+}
+console.log(somar(2, 2));
 
 // Função que subtrai dois números
-function subtrair() {}
+function subtrair(numero1, numero2) {
+  return numero1 - numero2;
+}
+console.log(subtrair(3,1));
 
 // Função que multiplica dois números
-function multiplicar() {}
+function multiplicar(numero1,numero2) {
+  return numero1 * numero2;
+}
+console.log(multiplicar(3,3));
 
 // Função que divide dois números, com verificação de divisão por zero
-function dividir() {}
+function dividir(numero1 , numero2) {
+  if (numero2 ===0){
+    return "não pode divisão por zero"
+  }
+  return numero1 / numero2
+}
+console.log(dividir(10,5));
 
 // Função que verifica se um número é par
-function ehPar() {}
+function ehPar(numero) {
+  if (numero === 0) {
+      return "Zero é par";
+  } 
+  if (numero % 2 === 0) {
+      return "É par";
+  } 
+  return "Erro: O número é ímpar.";
+  }
+console.log(ehPar(8));
 
 // Exporta todas as funções para serem usadas nos testes
 module.exports = {
@@ -21,3 +45,6 @@ module.exports = {
   dividir,
   ehPar,
 };
+
+/* Exercicios resolvidos e teste
+feito com sucesso */
